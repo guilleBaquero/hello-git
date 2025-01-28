bfea27a70a6aaaf646ee1e06c50e976f3d26fffc
guill <baqueroguille@gmail.com>
2025-01-28 20:36:38 +0100

    Cambio en hellogit.js

--- hellogit.js
+++ hellogit.js
@@ -1,1 +1,6 @@
-console.log("Hello Git!!!")
+console.log("Cambio en el fichero!")
+
+// git init // crear un repositorio en la raiz del proyecto
+// git status
+// git add hellogit.js
+// git commit -m "Mi primer commit"
