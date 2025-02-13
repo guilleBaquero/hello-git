4a15e7633f4aeb7b735ab0ff75abfb5365a37a59
guill <baqueroguille@gmail.com>
2025-02-13 10:41:35 +0100

    Último cambio

---
+++ hellogit.js
@@ -0,0 +1,28 @@
+console.log("New hello GitHub!")
+
+// git config --global user.name
+// git config --global user.email
+// git init // crear un repositorio en la raiz del proyecto
+// git status
+// git add hellogit.js
+// git commit -m "Mi primer commit"
+// git log
+// git checkout 1264a3b9d0058eadba80d2614b4be74ae7846339 // Ir a otro commit
+// git reset hellogit2.js
+// git config --global alias.tree "log --graph --decorate --all --oneline"
+// git tree
+// touch .gitignore
+// git diff
+// git reflog
+// git reset --hard hjvkuy76
+// git tag clase1
+// git checkout tags/clase1
+// git branch (-d)
+// git switch
+// git merge
+// git stash list / pop / drop
+// git remote add origin https://github.com/guilleBaquero/hello-git.git
+// git push -u origin main
+// git fetch // descargar en local el historial sin los cambios
+// git config pull.rebase false 
+// git pull origin main // historial con los cambios
